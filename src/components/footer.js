d312fea448dd50bb084d4b2c43a1354ae2de7a34
Javier Perez <jperez@spredfast.com>
2017-07-27 13:20:07 -0300

    Share

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import Avatar from 'material-ui/Avatar';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import Card, { CardContent } from 'material-ui/Card';
 import Chip from 'material-ui/Chip';
-
+import Share from './share';
 
 const styleSheet = createStyleSheet('Footer', theme => ({
   chip: {
@@ -34,32 +34,35 @@ class Footer extends React.Component {
   render() {
     const classes = this.props.classes;
     return (
-      <Card className={classes.card}>
-        <CardContent className={classes.cardContent}>
-          <Chip
-            avatar={
-              <Avatar
-                alt='Javier Perez'
-                src='https://www.gravatar.com/avatar/90fd523a31015aaa143278948e4729d1?s=200'
-              />
-            }
-            label='Javier Perez'
-            onClick={this.openJaviTwiter}
-            className={classes.chip}
-          />
-          <Chip
-            avatar={
-              <Avatar
-                alt='Santiago Fraire Willemoës'
-                src='https://www.gravatar.com/avatar/7ff595c207c9b935811a691d2f3d2551?s=200'
-              />
-            }
-            label='Santiago Fraire Willemoës'
-            onClick={this.openSanTwiter}
-            className={classes.chip}
-          />
-        </CardContent>
-      </Card>
+      <div>
+        <Share />
+        <Card className={classes.card}>
+          <CardContent className={classes.cardContent}>
+            <Chip
+              avatar={
+                <Avatar
+                  alt='Javier Perez'
+                  src='https://www.gravatar.com/avatar/90fd523a31015aaa143278948e4729d1?s=200'
+                />
+              }
+              label='Javier Perez'
+              onClick={this.openJaviTwiter}
+              className={classes.chip}
+            />
+            <Chip
+              avatar={
+                <Avatar
+                  alt='Santiago Fraire Willemoës'
+                  src='https://www.gravatar.com/avatar/7ff595c207c9b935811a691d2f3d2551?s=200'
+                />
+              }
+              label='Santiago Fraire Willemoës'
+              onClick={this.openSanTwiter}
+              className={classes.chip}
+            />
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
---
+++ src/components/share.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import { ShareButtons,  generateShareIcon } from 'react-share';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+} = ShareButtons;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const LinkedinIcon = generateShareIcon('linkedin');
+const PinterestIcon = generateShareIcon('pinterest');
+const VKIcon = generateShareIcon('vk');
+const OKIcon = generateShareIcon('ok');
+const TelegramIcon = generateShareIcon('telegram');
+const WhatsappIcon = generateShareIcon('whatsapp');
+const RedditIcon = generateShareIcon('reddit');
+
+const styleSheet = createStyleSheet('Share', theme => ({
+  buttons: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '1em 0',
+    flexWrap: 'wrap'
+  },
+  button: {
+    cursor: 'pointer',
+    margin: '0.5em'
+  }
+}));
+
+
+class Share extends Component {
+  render() {
+    const classes = this.props.classes;
+    //TODO: set these variable values using props
+    const shareUrl = window.location.href;
+    const title = 'Good Speech';
+    const picture = `${String(window.location)}/chrome.svg`;
+
+    return (
+      <div className={classes.buttons}>
+        <FacebookShareButton
+          url={shareUrl}
+          title={title}
+          picture={picture}
+          className={`shareButton ${classes.button}`}>
+          <FacebookIcon
+            size={32}
+            round />
+        </FacebookShareButton>
+
+        <TwitterShareButton
+          url={shareUrl}
+          title={title}
+          className={`shareButton ${classes.button}`}>
+          <TwitterIcon
+            size={32}
+            round />
+        </TwitterShareButton>
+
+        <TelegramShareButton
+          url={shareUrl}
+          title={title}
+          className={`shareButton ${classes.button}`}>
+          <TelegramIcon size={32} round />
+        </TelegramShareButton>
+
+        <WhatsappShareButton
+          url={shareUrl}
+          title={title}
+          separator=':: '
+          className={`shareButton ${classes.button}`}>
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+
+        <GooglePlusShareButton
+          url={shareUrl}
+          className={`shareButton ${classes.button}`}>
+          <GooglePlusIcon
+            size={32}
+            round />
+        </GooglePlusShareButton>
+
+        <LinkedinShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={750}
+          windowHeight={600}
+          className={`shareButton ${classes.button}`}>
+          <LinkedinIcon
+            size={32}
+            round />
+        </LinkedinShareButton>
+
+        <PinterestShareButton
+          url={shareUrl}
+          media={picture}
+          windowWidth={1000}
+          windowHeight={730}
+          className={`shareButton ${classes.button}`}>
+          <PinterestIcon size={32} round />
+        </PinterestShareButton>
+
+        <VKShareButton
+          url={shareUrl}
+          image={picture}
+          windowWidth={660}
+          windowHeight={460}
+          className={`shareButton ${classes.button}`}>
+          <VKIcon
+            size={32}
+            round />
+        </VKShareButton>
+
+        <OKShareButton
+          url={shareUrl}
+          image={picture}
+          windowWidth={660}
+          windowHeight={460}
+          className={`shareButton ${classes.button}`}>
+          <OKIcon
+            size={32}
+            round />
+        </OKShareButton>
+
+        <RedditShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={660}
+          windowHeight={460}
+          className={`shareButton ${classes.button}`}>
+          <RedditIcon
+            size={32}
+            round />
+        </RedditShareButton>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styleSheet)(Share);
